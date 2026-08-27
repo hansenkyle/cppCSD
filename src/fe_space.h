f764af4c959e4fc0cbe5718f4931375e2bccf1d6
Kyle Hansen <khansen3@ncsu.edu>
2026-08-27 12:55:31 -0400

    Add copyright boilerplate to all source and header files
    Prepends BSD 3-Clause copyright notice with CARRE funding attribution
    to every file in src/ and test/unit/.

--- src/fe_space.h
+++ src/fe_space.h
@@ -1,3 +1,10 @@
+// Copyright (c) 2026, Kyle Hansen (khansen3@ncsu.edu)
+//
+// Funded by CARRE (https://carre-psaapiv.org/)
+//
+// Licensed under BSD 3-Clause License; Redistribution and use in source and binary forms, with
+// or without modification are permitted provided that the terms of the license are met.
+
 #ifndef FE_SPACE_H
 #define FE_SPACE_H
 
