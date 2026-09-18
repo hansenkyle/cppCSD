005be4af01618835013a83b72db02821024f1e01
Kyle Hansen <khansen3@ncsu.edu>
2026-09-18 17:52:19 -0400

    Remove residuals.cpp/.h

--- src/residual.cpp
+++
@@ -1,5 +0,0 @@
-#include "residual.h"
-
-// residualNorm() is a template, so its definition lives entirely in
-// residual.h; this file exists to keep the header/source pairing this
-// codebase uses elsewhere, and to make sure the header compiles standalone.
